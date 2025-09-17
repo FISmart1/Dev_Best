08986b69c7577d161d02fba9cdce20adcbd8e01e
FISmart1 <yusufftibazma@gmail.com>
2025-09-17 22:26:04 +0700

    edit keahlian

--- src/components/EditSiswa.jsx
+++ src/components/EditSiswa.jsx
@@ -536,7 +536,7 @@ const EditSiswa = () => {
                       IT Support Assistant
                     </option>
                     <option value="IOT Engineer">IOT Engineer</option>
-                    <option value="Desain Grafis">Desain Grafis</option>
+                    <option value="Graphic Design">Graphic Design</option>
                     <option value="Photography">Photography</option>
                     <option value="Videography">Videography</option>
                   </select>
